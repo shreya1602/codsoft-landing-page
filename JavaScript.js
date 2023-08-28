c1853bf6dc370729b7757031f220f8ce52b2e26b
Shreya Kumari <71525741+shreya1602@users.noreply.github.com>
2023-08-28 22:56:27 +0530

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,9 @@
+// You can add JavaScript functionality here if needed.
+function openNavbar() {
+	document.getElementById("sideNavigationBar")
+		.style.width = "50%";
+}
+function closeNavbar() {
+	document.getElementById("sideNavigationBar")
+		.style.width = "0%";
+}
